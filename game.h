e1a614fe03ef008c3f0b5a7958b7317932170d37
Hehehasmr <pro1234plays@gmail.com>
2025-02-26 20:21:04 -0700

    game.h

---
+++ game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Graphics.hpp>
+#include "player.h"
+#include "zombie.h"
+#include "bullet.h"
+#include <vector>
+
+enum GameState { MENU, PLAY_EASY, PLAY_HARD, SHOP };
+
+class Game {
+private:
+    sf::RenderWindow window;
+    GameState gameState;
+    Player player;
+    std::vector<Zombie> zombies;
+    std::vector<Bullet> bullets;
+
+public:
+    Game();
+    void run();
+    void handleEvents();
+    void update();
+    void render();
+    void spawnZombies(int count);
+};
+
+#endif
